a9327d1270d34d5d27cf3ddf99335fac3c067212
ElMehdiJebbour <mehdi.jebb@gmail.com>
2023-12-06 18:02:23 +0100

    new changes

--- main.py
+++ main.py
@@ -16,16 +16,6 @@ class RecommendationRequest(BaseModel):
     #input_songs: List[str]
     #top_n: int = 10
 
-@app.post("/recommendForUser")
-async def recommend_songs(request: RecommendationRequest):
-    try:
-        user_id = request.user_id
-        n_recommendations = request.n_recommendations
-        recommendations = recommend_songs_for_user(model_knn, pivot_table, user_id, n_recommendations)
-
-        return {"user_id": user_id, "recommendations": recommendations}
-    except Exception as e:
-        return JSONResponse(content={"error": str(e)}, status_code=500)
 
 
 # Initialize Firestore client
@@ -105,54 +95,5 @@ async def get_all_users():
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.post("/write_random_valhalla")
-async def write_random_valhalla():
-    try:
-        # Reference to the collection (e.g., 'data')
-        collection_ref = db.collection("users")
-
-        # Generate a random value (customize as needed)
-        random_value = random.randint(1, 100)  # Example: random integer between 1 and 100
-
-        # Create a new document with the random value under the key 'valhalla'
-        doc_ref = collection_ref.add({"valhalla": random_value})
-
-        # Fetch the generated document ID
-        document_id = doc_ref[1].id
-
-        return {"document_id": document_id, "valhalla": random_value}
-
-    except Exception as e:
-        import traceback
-        traceback.print_exc()
-        raise HTTPException(status_code=500, detail=str(e))
     
     
-    
-@app.post("/add_song_to_playlist/{user_id}")
-async def add_song_to_playlist(user_id: str, song_id: str = Body(..., embed=True)):
-    try:
-        # Reference to the user's document
-        user_doc_ref = db.collection("users").document(user_id)
-        user_doc = user_doc_ref.get()
-
-        if user_doc.exists:
-            # Fetch the user's playlist
-            user_data = user_doc.to_dict()
-            playlist = user_data.get("playlist", {})
-
-            # Update the playlist with the new song
-            # Assuming a simple structure where the value is a count of how many times the song is added
-            playlist[song_id] = playlist.get(song_id, 0) + 1
-
-            # Store the updated playlist back in Firestore
-            user_doc_ref.update({"playlist": playlist})
-
-            return {"user_id": user_id, "song_id": song_id, "playlist": playlist}
-        else:
-            raise HTTPException(status_code=404, detail="User not found")
-
-    except Exception as e:
-        import traceback
-        traceback.print_exc()
-        raise HTTPException(status_code=500, detail=str(e))
